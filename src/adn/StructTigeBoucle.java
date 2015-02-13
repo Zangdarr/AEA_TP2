6db2a056655cebe0e06068b47c6d99912350f122
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-13 11:09:52 +0100

    StructTigeBoucle.java : Ajout des attributs.
    Ces attribut on été tiré de la fiche TP. Ces données serviront de spécification à l'algorithme de génération de cette structure. D'autres attributs informatifs ou contraintes pourront être ajoutés si besoin.

--- src/adn/StructTigeBoucle.java
+++ src/adn/StructTigeBoucle.java
@@ -1,5 +1,16 @@
 package adn;
 
 public class StructTigeBoucle {
-
+    
+    // Nombre de nucléotides que contient la structure
+    int taille;
+    // Nombre d'appariements que contient la structure
+    int nombre_appariements;
+    // Taille de la boucle terminal situé au mileu de la structure
+    int taille__boucle_terminal;
+    // Quantité de boucles autres que la terminal
+    int nombre_autres_boucles;
+    
+    
+    
 }
