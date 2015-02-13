507d248ddaa2cfc5af83c068015a4bbc15d5a379
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-13 15:07:26 +0100

    StructTigeBoucle.java : Ajout du constructeur par defaut et du constructeur avec paramètre.
    Le constructeur par défaut initialise tous les paramètres de façon aléatoire bornée par les contraintes.
    Le constructeur avec paramètre peut observer 3 situations :
    1) un appel avec des paramètres valides pour chaque,
    2) un appel avec des paramètres avec des valeurs négatif pour certains : les paramètre à valeur négatif seront choisis aléatoirement
    3) un appel avec des paramètres invalides (valeur trop petite par rapport au contrainte ou trop grande...) : la valeur sera arrondie à la contrainte la plus proche.
    
    En ce qui concerne le nombre de boucle latéral, la valeur à été mise à zéro pour évité de s'attarder dessus. Une fois que le système fonctionnera, on le complexifiera avec cette contrainte.

--- src/adn/StructTigeBoucle.java
+++ src/adn/StructTigeBoucle.java
@@ -1,5 +1,7 @@
 package adn;
 
+import java.util.Random;
+
 public class StructTigeBoucle {
     
     /**
@@ -25,6 +27,69 @@ public class StructTigeBoucle {
     int taille__boucle_terminal;
     // Quantité de boucles autres que la terminal
     int nombre_autres_boucles;
+    
+    
+    
+    /**
+     * Constructeur par default. Toute les valeur initiale sont aléatoire.
+     */
+    public StructTigeBoucle() {
+        super();
+        Random rnd = new Random(System.currentTimeMillis());
+
+        this.taille = rnd.nextInt(1 + MAX_LONGUEUR);
+        this.nombre_appariements = MIN_NB_APPARIEMENT + rnd.nextInt(1 +  this.taille/2 - MIN_NB_APPARIEMENT);
+        this.taille__boucle_terminal = rnd.nextInt(1 + MAX_TAILLE_BOUCLE_TERMINAL);
+        
+        //TODO : Pour l'instant ce paramètre sera à zéro. La gestion des boucles latérales se fera plus tard.
+        this.nombre_autres_boucles = 0;
+    }
+
+
+
+    /**
+     * Constructeur : si une valeur négative est passé en paramètre, un nombre aléatoire borné par les MIN et MAX sera généré pour la paramètre correspondant.
+     * @param taille
+     * Si taille est inférieur à la longueur minimale imposée, alors la valeur minimale sera appliquée
+     * @param nombre_appariements
+     * Si le nombre d'appariement est inférieur au nombre minimal imposé, alors la valeur minimale sera appliquée
+     * @param taille__boucle_terminal
+     * Si la taille dépasse la valeur maximale, alors la valeur maximale sera appliquée
+     * @param nombre_autres_boucles
+     * Ce paramètre sera gérer plus tard. Pour limiter la complexité du programme pour l'instant.
+     */
+    public StructTigeBoucle(int taille, int nombre_appariements,
+            int taille__boucle_terminal, int nombre_autres_boucles) {
+        super();
+        Random rnd = new Random(System.currentTimeMillis());
+        
+        this.taille = (taille<0)
+                            ?MIN_LONGUEUR + rnd.nextInt(1 + MAX_LONGUEUR - MIN_LONGUEUR)
+                            : (taille<MIN_LONGUEUR)
+                                    ?MIN_LONGUEUR
+                                    :taille
+        ;
+                                                 
+        this.nombre_appariements = (nombre_appariements<0)
+                                                ?MIN_NB_APPARIEMENT + rnd.nextInt((1 +  this.taille/2 - MIN_NB_APPARIEMENT))
+                                                : (taille<MIN_NB_APPARIEMENT)
+                                                        ? MIN_NB_APPARIEMENT
+                                                        : nombre_appariements 
+        ;
+        
+        this.taille__boucle_terminal = (taille__boucle_terminal<0)
+                                                ? rnd.nextInt(1 + MAX_TAILLE_BOUCLE_TERMINAL)
+                                                : (taille__boucle_terminal>MAX_TAILLE_BOUCLE_TERMINAL)
+                                                        ? MAX_TAILLE_BOUCLE_TERMINAL
+                                                        : taille__boucle_terminal
+        ;
+
+        //TODO : Pour l'instant ce paramètre sera à zéro. La gestion des boucles latérales se fera plus tard.
+        this.nombre_autres_boucles = 0;
+        ;
+    }
+    
+    
     
     //GETTER - SETTER    
     public int getTaille() {
