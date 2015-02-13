35eaa0032493c27b1d497548ce34d5d8d653960b
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-13 11:11:58 +0100

    StructTigeBoucle.java : Ajout des premiers getter de base.

--- src/adn/StructTigeBoucle.java
+++ src/adn/StructTigeBoucle.java
@@ -11,6 +11,20 @@ public class StructTigeBoucle {
     // Quantité de boucles autres que la terminal
     int nombre_autres_boucles;
     
+    //GETTER - SETTER    
+    public int getTaille() {
+        return taille;
+    }
+    public int getNombre_appariements() {
+        return nombre_appariements;
+    }
+    public int getTaille__boucle_terminal() {
+        return taille__boucle_terminal;
+    }
+    public int getNombre_autres_boucles() {
+        return nombre_autres_boucles;
+    }
+    
     
     
 }
