1c17598dbff2830c2c9d7eb7b961822c2d054798
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-03 14:32:37 +0100

    Ajout dans PreMicArnGenerator.java : ajout d'une méthode qui génère aléatoirement une paire de nucléotides ne suivant pas la complémentarité faible A-U, G-C, G-U.

--- src/adn/PreMicArnGenerator.java
+++ src/adn/PreMicArnGenerator.java
@@ -36,6 +36,21 @@ public class PreMicArnGenerator {
     
 
 
+    private String genPaireNonApparieNucleotide() {
+        Random rnd = new Random(System.currentTimeMillis());
+        int paire_choisie = rnd.nextInt(3);
+        int ordre_choisi  = rnd.nextInt(2);
+        String result = "";
+        switch(paire_choisie){
+        case 0 : 
+            result = (ordre_choisi == 0)?"AU" : "UA";
+        case 1 :
+            result = (ordre_choisi == 0)?"GC" : "CG";
+        case 2 :
+            result = (ordre_choisi == 0)?"GU" : "UG";
+        }
+        return result;
+    }
 
     /**
      * Génère une paire de nucléotides apparieable aléatoirement
