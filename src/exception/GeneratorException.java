a1440c01c41f954a03de33456250e2e460a31755
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-03 15:57:19 +0100

    MAJ dans GeneratorException.java : Spécification du comportement par defaut de l'exception.

--- src/exception/GeneratorException.java
+++ src/exception/GeneratorException.java
@@ -12,6 +12,10 @@ public class GeneratorException extends Exception {
         case 0 : 
             message = "Trop de tentatives de génération on échouées. Suggestion : changer de structure TigeBoucle.";
             throw new GeneratorException(message);
+        default : 
+            message = "Une erreur est survenue lors de la génération du Pré-Micro ARN";
+            throw new GeneratorException(message);
+
         }
 
     }
