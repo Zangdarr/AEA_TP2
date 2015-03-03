aa42ddc06cef58773438326e1c35cdd06e94a9dc
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-03 15:58:05 +0100

    MAJ dans GeneratorException.java : spécification du comprtement pour un nouveau coas d'exception.

--- src/exception/GeneratorException.java
+++ src/exception/GeneratorException.java
@@ -12,6 +12,9 @@ public class GeneratorException extends Exception {
         case 0 : 
             message = "Trop de tentatives de génération on échouées. Suggestion : changer de structure TigeBoucle.";
             throw new GeneratorException(message);
+        case 1 :
+            message = "Mauvais nom de nucléotides passé en paramètre";
+            throw new GeneratorException(message);
         default : 
             message = "Une erreur est survenue lors de la génération du Pré-Micro ARN";
             throw new GeneratorException(message);
