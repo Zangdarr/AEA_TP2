f5cd5d9b78626bb318542b3eb66a0386ace5d6b8
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-03 14:11:58 +0100

    Agencement PreMicArnGenerator.java : ajout du "squelette" d'espace dans la classe (cela simplifiera les commits qui arrivent)

--- src/adn/PreMicArnGenerator.java
+++ src/adn/PreMicArnGenerator.java
@@ -1,23 +1,31 @@
 package adn;
 
+
+
 public class PreMicArnGenerator {
-    
+
     /**
      * Contrainte de la generation
      */
     private StructTigeBoucle contrainte;
 
-    
-    
+
+
+
     public PreMicArnGenerator(StructTigeBoucle contrainte) {
         super();
         this.contrainte = contrainte;
     }
+
     
     public String generate(){
              
         return null;
     }
     
+
+
+
+
 
 }
