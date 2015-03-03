94089e4b41e4194a30bf91ee35fd134adce557cc
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-03 14:56:08 +0100

    Ajout dans le repository : ajout d'un main pour tester la génération des PréMicArn.

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import exception.GeneratorException;
+import adn.PreMicArnGenerator;
+import adn.StructTigeBoucle;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        StructTigeBoucle struct = new StructTigeBoucle();
+        
+        PreMicArnGenerator gen = new PreMicArnGenerator(struct);
+        
+        //Nombre de fois que sera généré un microARN à partir de la structure TigeBoucle "struct"
+        int nombre_de_fois = 10;
+        String[] tab = new String[nombre_de_fois];
+        
+        for (int i = 0; i< tab.length;i++) {
+            try {
+                tab[i] = gen.generate().toString();
+                System.out.println(tab[i] + "\n");
+            } catch (GeneratorException e) {
+                System.err.println(e.getMessage());
+                break;
+            }
+        }
+        
+        System.out.println("\n\n\n");
+        struct.printCaracteristics();
+       
+    }
+
+}
