b91b8e40fc22c0c5c135a575a15f7f251c21af2b
qw0te <baert.quentin@gmail.com>
2015-03-06 20:17:14 +0100

    Ajoute un main qui permet de générer une structure tige/boucle depuis quelques paramètres

---
+++ src/main/GenerationTigeBoucleMain.java
@@ -0,0 +1,41 @@
+package main;
+
+import adn.PreMicArnGenerator;
+import adn.StructTigeBoucle;
+import exception.GeneratorException;
+
+public class GenerationTigeBoucleMain {
+
+	public static void printInfo() {
+		System.out.println("java -jar structGenerator.jar <tailleSequence> <nb> <tailleTerm>");
+		System.out.println();
+		System.out.println("Avec :");
+		System.out.println("\t\t\t <tailleSequence> : taille de la structure à générer");
+		System.out.println("\t\t\t <nb>             : nombre de boucles internes");
+		System.out.println("\t\t\t <tailleTerm>     : taille de la boucle terminale");
+	}
+	public static void main(String[] args) throws GeneratorException {
+		if (args.length < 3) {
+			printInfo();
+			return;
+		}
+		
+		try {
+			int tailleSequence = Integer.parseInt(args[0]);
+			int nb = Integer.parseInt(args[1]);
+			int tailleTerm = Integer.parseInt(args[2]);
+
+        	StructTigeBoucle struct = new StructTigeBoucle(tailleSequence, nb, tailleTerm);
+        
+        	PreMicArnGenerator gen = new PreMicArnGenerator(struct);
+        	String skeleton = gen.generate();
+        
+        	System.out.println(skeleton);
+        	System.out.println(gen.convertBoneToPreMiC(new StringBuffer(skeleton)).toString());
+		} catch (NumberFormatException e) {
+			printInfo();
+			return;
+		}
+	}
+
+}
